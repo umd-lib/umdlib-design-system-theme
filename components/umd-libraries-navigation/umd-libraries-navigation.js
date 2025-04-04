372bcf3b97ebfa508bb29149d8f4d45f4b0fdd39
Alfred J Lin <alfred.jr.lin@gmail.com>
2025-04-04 15:58:13 -0400

    LIBWEB-6306 Navigation Component
    - fix javascript issue in sandbox
    - update styling for UMD school banner
    - add link to the logo
    
    https://umd-dit.atlassian.net/browse/LIBWEB-6306

--- components/umd-libraries-navigation/umd-libraries-navigation.js
+++ components/umd-libraries-navigation/umd-libraries-navigation.js
@@ -1,90 +1,79 @@
-(function (Drupal) {
-  Drupal.behaviors.menuToggle = {
-    attach: function (context) {
-      context
-        .querySelectorAll(".navigation__menu-button")
-        .forEach(function (button) {
-          // Prevent duplicate event listeners
-          if (!button.hasAttribute("data-processed")) {
-            button.setAttribute("data-processed", "true");
+document.addEventListener("DOMContentLoaded", function () {
+  // Menu Toggle
+  document
+    .querySelectorAll(".navigation__menu-button")
+    .forEach(function (button) {
+      if (!button.hasAttribute("data-processed")) {
+        button.setAttribute("data-processed", "true");
 
-            button.addEventListener("click", function () {
-              // update menu button style
-              this.classList.toggle("is-active");
-              this.setAttribute(
-                "aria-expanded",
-                this.getAttribute("aria-expanded") === "true" ? "false" : "true"
-              );
+        button.addEventListener("click", function () {
+          // Update menu button style
+          this.classList.toggle("is-active");
+          this.setAttribute(
+            "aria-expanded",
+            this.getAttribute("aria-expanded") === "true" ? "false" : "true"
+          );
 
-              // update menu - open/close
-              const navigationRows =
-                document.querySelector(".navigation__rows");
-              const navigationSubmenu = document.querySelectorAll(
-                ".navigation__submenu"
-              );
+          // Update menu - open/close
+          const navigationRows = document.querySelector(".navigation__rows");
+          const navigationSubmenu = document.querySelectorAll(
+            ".navigation__submenu"
+          );
 
-              if (navigationRows) {
-                navigationRows.classList.toggle("is-open");
-              }
-
-              navigationSubmenu.forEach(function (submenu) {
-                submenu.classList.toggle("is-open");
-                if (submenu.classList.contains("is-active")) {
-                  submenu.classList.remove("is-active");
-                }
-              });
-            });
-          }
-        });
-    },
-  };
-
-  Drupal.behaviors.submenuToggle = {
-    attach: function (context) {
-      context
-        .querySelectorAll(".navigation__submenu-button")
-        .forEach(function (button) {
-          if (!button.hasAttribute("data-processed")) {
-            button.setAttribute("data-processed", "true");
+          if (navigationRows) {
+            navigationRows.classList.toggle("is-open");
           }
 
-          const elementID = button.getAttribute("aria-controls");
-
-          button.addEventListener("click", function () {
-            this.setAttribute(
-              "aria-expanded",
-              this.getAttribute("aria-expanded") === "true" ? "false" : "true"
-            );
-
-            const targetSubmenu = document.getElementById(elementID);
-
-            if (targetSubmenu) {
-              targetSubmenu.classList.toggle("is-active");
+          navigationSubmenu.forEach(function (submenu) {
+            submenu.classList.toggle("is-open");
+            if (submenu.classList.contains("is-active")) {
+              submenu.classList.remove("is-active");
             }
           });
         });
-    },
-  };
+      }
+    });
 
-  Drupal.behaviors.backButton = {
-    attach: function (context) {
-      context
-        .querySelectorAll(".navigation__back-button")
-        .forEach(function (button) {
-          if (!button.hasAttribute("data-processed")) {
-            button.setAttribute("data-processed", "true");
-          }
+  // Submenu Toggle
+  document
+    .querySelectorAll(".navigation__submenu-button")
+    .forEach(function (button) {
+      if (!button.hasAttribute("data-processed")) {
+        button.setAttribute("data-processed", "true");
+      }
 
-          button.addEventListener("click", function () {
-            const navigationSubmenu = document.querySelectorAll(
-              ".navigation__submenu"
-            );
+      const elementID = button.getAttribute("aria-controls");
 
-            navigationSubmenu.forEach(function (submenu) {
-              submenu.classList.remove("is-active");
-            });
-          });
+      button.addEventListener("click", function () {
+        this.setAttribute(
+          "aria-expanded",
+          this.getAttribute("aria-expanded") === "true" ? "false" : "true"
+        );
+
+        const targetSubmenu = document.getElementById(elementID);
+
+        if (targetSubmenu) {
+          targetSubmenu.classList.toggle("is-active");
+        }
+      });
+    });
+
+  // Back Button
+  document
+    .querySelectorAll(".navigation__back-button")
+    .forEach(function (button) {
+      if (!button.hasAttribute("data-processed")) {
+        button.setAttribute("data-processed", "true");
+      }
+
+      button.addEventListener("click", function () {
+        const navigationSubmenu = document.querySelectorAll(
+          ".navigation__submenu"
+        );
+
+        navigationSubmenu.forEach(function (submenu) {
+          submenu.classList.remove("is-active");
         });
-    },
-  };
-})(Drupal);
+      });
+    });
+});
